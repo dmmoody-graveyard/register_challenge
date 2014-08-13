f94815e9bf3e5ed411bdbc4700b5d3fccba6ec5e
Duane M Moody <dmmoody@gmail.com>
2014-08-13 13:54:44 -0700

    Added quantity to products area

--- js/app.js
+++ js/app.js
@@ -3,31 +3,31 @@ var myApp = angular.module('myApp', []);
 myApp.controller('CartForm', ['$scope', function ($scope) {
     $scope.productList = [
         {
-        qty: 0,
-        name: 'Product A',
-        cost: 20.00,
-        discount_qty: [0,0],
-        subtotal: 0,
-        edit_price: false},
+        'qty': 0,
+        'name': 'Product A',
+        'cost': 20.00,
+        'discount_qty': [0,0],
+        'subtotal': 0,
+        'edit_price': false},
         {
-        qty: 0,
-        name: 'Product B',
-        cost: 50.00,
-        discount_qty: [5,3],
-        subtotal: 0,
-        edit_price: false},
+        'qty': 0,
+        'name': 'Product B',
+        'cost': 50.00,
+        'discount_qty': [5,3],
+        'subtotal': 0,
+        'edit_price': false},
         {
-        qty: 0,
-        name: 'Product C',
-        cost: 30.00,
-        discount_qty: [0,0],
-        subtotal: 0,
-        edit_price: false}
+        'qty': 0,
+        'name': 'Product C',
+        'cost': 30.00,
+        'discount_qty': [0,0],
+        'subtotal': 0,
+        'edit_price': false}
     ];
     
     $scope.cartList = [
         
-        ];
+    ];
     
     $scope.addItem = function(item) {
         $scope.cartList.push(item);
@@ -39,7 +39,7 @@ myApp.controller('CartForm', ['$scope', function ($scope) {
     
     $scope.total = function() {
         var total = 0;
-        angular.forEach($scope.productList, function(item) {
+        angular.forEach($scope.cartList, function(item) {
             var savings = Math.floor(item.qty / item.discount_qty[0]) * item.cost * (item.discount_qty[0] - item.discount_qty[1]);
             if (item.discount_qty[0] && Math.floor(item.qty / item.discount_qty[0])) {
                 item.subtotal = item.qty * item.cost - savings;
