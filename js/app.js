fba76962e229e6158814991cabc2a635ce0b5782
Duane M Moody <dmmoody@gmail.com>
2014-08-13 10:50:54 -0700

    Changed list of products var name from invoice to productList

--- js/app.js
+++ js/app.js
@@ -1,44 +1,50 @@
 var myApp = angular.module('myApp', []);
 
 myApp.controller('CartForm', ['$scope', function ($scope) {
-        $scope.invoice = {
-            items: [{
-                qty: 0,
-                name: 'Product A',
-                cost: 20.00,
-                discount_qty: [0,0],
-                subtotal: 0,
-                edit_price: false},
-                {
-                qty: 0,
-                name: 'Product B',
-                cost: 50.00,
-                discount_qty: [5,3],
-                subtotal: 0,
-                edit_price: false},
-                {
-                qty: 0,
-                name: 'Product C',
-                cost: 30.00,
-                discount_qty: [0,0],
-                subtotal: 0,
-                edit_price: false}
-                   ]
-        };
+    $scope.productList = {
+        items: [{
+            qty: 0,
+            name: 'Product A',
+            cost: 20.00,
+            discount_qty: [0,0],
+            subtotal: 0,
+            edit_price: false},
+            {
+            qty: 0,
+            name: 'Product B',
+            cost: 50.00,
+            discount_qty: [5,3],
+            subtotal: 0,
+            edit_price: false},
+            {
+            qty: 0,
+            name: 'Product C',
+            cost: 30.00,
+            discount_qty: [0,0],
+            subtotal: 0,
+            edit_price: false}
+               ]
+    };
+    
+    $scope.cartList = {
+        
+    };
+    
+    $scope.total = function() {
+        var total = 0;
+        angular.forEach($scope.productList.items, function(item) {
+            var savings = Math.floor(item.qty / item.discount_qty[0]) * item.cost * (item.discount_qty[0] - item.discount_qty[1]);
+            if (item.discount_qty[0] && Math.floor(item.qty / item.discount_qty[0])) {
+                item.subtotal = item.qty * item.cost - savings;
+                total += item.subtotal;
+            } else {
+                item.subtotal = item.qty * item.cost;
+                total += item.subtotal;
+            }
+        })
 
-        $scope.total = function() {
-            var total = 0;
-            angular.forEach($scope.invoice.items, function(item) {
-                var savings = Math.floor(item.qty / item.discount_qty[0]) * item.cost * (item.discount_qty[0] - item.discount_qty[1]);
-                if (item.discount_qty[0] && Math.floor(item.qty / item.discount_qty[0])) {
-                    item.subtotal = item.qty * item.cost - savings;
-                    total += item.subtotal;
-                } else {
-                    item.subtotal = item.qty * item.cost;
-                    total += item.subtotal;
-                }
-            })
+        return total;
+    }
 
-            return total;
-        }
-    }]);
+
+}]);
